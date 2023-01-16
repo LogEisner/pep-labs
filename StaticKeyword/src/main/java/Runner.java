4650a07daad7cfe96f814164f1486e4a80e5b036
LogEisner <loganeisner@yahoo.com>
2023-01-16 14:27:03 +0000

    Saved progress on CreateTable 2023-01-16 14:27:03 +0000

--- StaticKeyword/src/main/java/Runner.java
+++ StaticKeyword/src/main/java/Runner.java
@@ -22,6 +22,8 @@ public class Runner {
      * @param in the String value you should change global_var to in.
      */
     public void changeClassVar(String in){
+        in = "xyz";
+        Class.global_var = in;
 
     }
 }
